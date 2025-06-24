cad0af67f883bc14e22a9259866175270fb93cd3
ExoticSliceOfPizza <136937744+PizzaLvr49@users.noreply.github.com>
2025-06-23 23:41:01 -0400

    thats enough for tonight

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,27 @@
-export function makeHello(name: string) {
-	return `Hello from ${name}!`;
+import { DataStoreService } from "@rbxts/services";
+import SchemaStoreSession from "./SchemaStoreSession";
+
+type DataSchema = { [key: string]: DataSchema } | number | string | boolean | DataSchema[];
+
+export class SchemaStore<Schema extends DataSchema> {
+	private readonly template: Schema;
+	private readonly ApiStore: DataStore;
+
+	constructor(storeName: string, template: Schema) {
+		this.template = template;
+		this.ApiStore = DataStoreService.GetDataStore("___SS", storeName);
+	}
+
+	ReadAsync(player: Player): Schema {
+		const data = this.ApiStore.GetAsync(`Player_${player.UserId}`);
+		print(data);
+
+		return (data as unknown as Schema) ?? this.template; // fix later lol
+	}
+
+	StartSessionAsync(key: string): SchemaStoreSession {
+		return new SchemaStoreSession();
+	}
 }
+
+new SchemaStore<{ hello: string }>("", { hello: "" });
